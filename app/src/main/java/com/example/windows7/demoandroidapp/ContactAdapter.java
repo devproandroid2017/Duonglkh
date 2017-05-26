f4921c87cfbe217008c59cba9c69af016a6f32f5
duonglkhph <hungduong16100@gmail.com>
2017-05-26 19:54:01 +0700

    Duong

---
+++ app/src/main/java/com/example/windows7/demoandroidapp/ContactAdapter.java
@@ -0,0 +1,75 @@
+package com.example.windows7.demoandroidapp;
+
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Windows 7 on 5/26/2017.
+ */
+
+public class ContactAdapter extends BaseAdapter {
+
+    ArrayList<List>contacts;
+
+    public ContactAdapter(ArrayList<List>contacts){
+        this.contacts = contacts;
+    }
+
+    @Override
+    public int getCount() {
+        return contacts.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return contacts.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View rowView = inflater.inflate(R.layout.listitem_row, parent, false);
+
+        ImageView imgAva = (ImageView) rowView.findViewById(R.id.avatar);
+
+        TextView tvName = (TextView) rowView.findViewById(R.id.txtName);
+        TextView tvCPU = (TextView) rowView.findViewById(R.id.txtCPU);
+        TextView tvMota = (TextView) rowView.findViewById(R.id.txtMota);
+
+        Contact contact = (Contact) contacts.get(position);
+        tvName.setText(contact.name);
+        tvCPU.setText(contact.cpu);
+        tvMota.setText(contact.mota);
+        if(contact.type.equals("1")){
+            imgAva.setImageResource(R.drawable.lich);
+        }else if (contact.type.equals("2")) {
+            imgAva.setImageResource(R.drawable.danhba);
+        }else if (contact.type.equals("3")) {
+            imgAva.setImageResource(R.drawable.fb);
+        }else if (contact.type.equals("4")) {
+            imgAva.setImageResource(R.drawable.tw);
+        }else if (contact.type.equals("5")) {
+            imgAva.setImageResource(R.drawable.tinnhan);
+        }else if (contact.type.equals("6")) {
+            imgAva.setImageResource(R.drawable.tudien);
+        }else if (contact.type.equals("7")) {
+            imgAva.setImageResource(R.drawable.nhac);
+        }else if (contact.type.equals("8")) {
+            imgAva.setImageResource(R.drawable.blue);
+        }
+        return rowView;
+    }
+}
--- app/src/main/java/com/example/windows7/demoandroidapp/Main2Activity.java
+++ app/src/main/java/com/example/windows7/demoandroidapp/Main2Activity.java
@@ -2,12 +2,34 @@ package com.example.windows7.demoandroidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main2Activity extends AppCompatActivity {
+
+    ArrayList<List>contacts = new ArrayList<>();
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        contacts.add(new Contact("Calendar","System", "CPU : 0%", "1"));
+        contacts.add(new Contact("Contacts","System", "CPU : 0%", "2"));
+        contacts.add(new Contact("Facebook","Background", "CPU : 28%", "3"));
+        contacts.add(new Contact("Twitter","Background", "CPU : 0%", "4"));
+        contacts.add(new Contact("Messaging","System", "CPU : 0%", "5"));
+        contacts.add(new Contact("Dictionary Provider","System", "CPU : 0%", "6"));
+        contacts.add(new Contact("Music FX","System", "CPU : 0%", "7"));
+        contacts.add(new Contact("Nfc Service","System", "CPU : 0%", "8"));
+
+        ContactAdapter contactAdapter = new ContactAdapter(contacts);
+        listView.setAdapter(contactAdapter);
     }
 }
